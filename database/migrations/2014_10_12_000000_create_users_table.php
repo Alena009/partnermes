9d36a1869cc6c6e82bbef5d42d5a8354bf6bb391
pawel_filip <pawel@filip@interia.pl>
2019-05-15 17:56:47 +0200

    połącznone migracje tabeli users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('kod', '45')->unique();
-            $table->string('firstname', '45');
-            $table->string('lastname', '45');
-            $table->string('login', '45')->unique();
-            $table->string('password', '155');                       
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
---
+++ database/migrations/2019_05_15_174700_change_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('kod', '45')->unique();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('login')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('kod');
+            $table->dropColumnt('firstname');
+            $table->dropColumn('lastname');
+            $table->dropColumn('login');
+        });
+    }
+}
